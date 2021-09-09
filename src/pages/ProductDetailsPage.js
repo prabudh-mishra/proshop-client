85ad39ec79dc0ceb41b9139b252a488a3d79fca9
Prabudh Mishra <prabudh.mishra18@gmail.com>
2021-09-09 16:23:07 +0530

    Implemented redux store in react app and fetch products and their details from global store

--- src/pages/ProductDetailsPage.js
+++ src/pages/ProductDetailsPage.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import Rating from '../components/Rating';
+import { getProductDetails } from '../data/actions/productActions';
 
-import products from '../products';
+import Rating from '../components/Rating';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const ProductDetailsPage = ({ match }) => {
-	const product = products.find((product) => product._id === match.params.id);
+	const dispatch = useDispatch();
+
+	const productDetails = useSelector((state) => state.productDetails);
+	const { product, loading, error } = productDetails;
+
+	useEffect(() => {
+		dispatch(getProductDetails(match.params.id));
+	}, [dispatch, match]);
 
 	return (
 		<>
@@ -15,57 +26,72 @@ const ProductDetailsPage = ({ match }) => {
 				Go Back
 			</Link>
 
-			<Row>
-				<Col md={6}>
-					<Image src={product.image} alt={product.name} />
-				</Col>
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message variant='danger'>{error}</Message>
+			) : (
+				<Row>
+					<Col md={6}>
+						<Image
+							className='product-image'
+							src={product.image}
+							alt={product.name}
+						/>
+					</Col>
 
-				<Col md={3}>
-					<ListGroup variant='flush'>
-						<ListGroup.Item>
-							<h3>{product.name}</h3>
-						</ListGroup.Item>
-						<ListGroup.Item>
-							<Rating
-								value={product.rating}
-								text={`${product.numReviews} reviews`}
-							/>
-						</ListGroup.Item>
-						<ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-						<ListGroup.Item>Description: {product.description}</ListGroup.Item>
-					</ListGroup>
-				</Col>
-
-				<Col md={3}>
-					<Card>
+					<Col md={3}>
 						<ListGroup variant='flush'>
 							<ListGroup.Item>
-								<Row>
-									<Col>Price:</Col>
-									<Col>
-										<strong>${product.price}</strong>
-									</Col>
-								</Row>
+								<h3>{product.name}</h3>
 							</ListGroup.Item>
 							<ListGroup.Item>
-								<Row>
-									<Col>Status:</Col>
-									<Col>
-										<strong>
-											{product.countInStock > 0 ? 'In Stock' : 'Out of stock'}
-										</strong>
-									</Col>
-								</Row>
+								<Rating
+									value={product.rating}
+									text={`${product.numReviews} reviews`}
+								/>
 							</ListGroup.Item>
+							<ListGroup.Item>Price: ${product.price}</ListGroup.Item>
 							<ListGroup.Item>
-								<Button className='w-100' disabled={product.countInStock === 0}>
-									Add to cart
-								</Button>
+								Description: {product.description}
 							</ListGroup.Item>
 						</ListGroup>
-					</Card>
-				</Col>
-			</Row>
+					</Col>
+
+					<Col md={3}>
+						<Card>
+							<ListGroup variant='flush'>
+								<ListGroup.Item>
+									<Row>
+										<Col>Price:</Col>
+										<Col>
+											<strong>${product.price}</strong>
+										</Col>
+									</Row>
+								</ListGroup.Item>
+								<ListGroup.Item>
+									<Row>
+										<Col>Status:</Col>
+										<Col>
+											<strong>
+												{product.countInStock > 0 ? 'In Stock' : 'Out of stock'}
+											</strong>
+										</Col>
+									</Row>
+								</ListGroup.Item>
+								<ListGroup.Item>
+									<Button
+										className='w-100'
+										disabled={product.countInStock === 0}
+									>
+										Add to cart
+									</Button>
+								</ListGroup.Item>
+							</ListGroup>
+						</Card>
+					</Col>
+				</Row>
+			)}
 		</>
 	);
 };
